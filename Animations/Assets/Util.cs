1c1fb87417dd60995020b7d53a50d7fa325b42f1
STUDENT Darragh M. Quinn <Darragh.Quinn@students.ittralee.ie>
2024-12-05 17:59:02 +0000

    Signed-off-by: STUDENT Darragh M. Quinn <Darragh.Quinn@students.ittralee.ie>

---
+++ Animations/Assets/Util.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util
+{
+    public static Vector3 ProjectVectorOntoNormal(Vector3 vector, Vector3 normal)
+    {
+        return Vector3.Dot(vector, normal) * normal;
+    }
+
+    public static Vector3 ExtractComponentPerpendicularToNormal(Vector3 vector, Vector3 normal)
+    {
+        return vector - ProjectVectorOntoNormal(vector, normal);
+    }
+}
